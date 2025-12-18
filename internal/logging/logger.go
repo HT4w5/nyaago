22c7dfa3c3b2f5a33995d9ed3f34d12b17fcea71
HT4w5 <ht4w5x@gmail.com>
2025-12-18 23:11:35 +0800

    feat(logging): add logger setup

---
+++ internal/logging/logger.go
@@ -0,0 +1,64 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/HT4w5/nyaago/internal/config"
+)
+
+const (
+	LoggerKeyModule = "module"
+)
+
+func SetupLogger(cfg *config.LogConfig) (*slog.Logger, error) {
+	var writer io.Writer
+
+	switch cfg.Access {
+	case "none":
+		return slog.New(slog.DiscardHandler), nil
+	case "":
+		writer = os.Stdout
+	default:
+		f, err := os.OpenFile(cfg.Access, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open log file %s: %w", cfg.Access, err)
+		}
+		writer = f
+	}
+
+	var handler slog.Handler
+	var level slog.Level
+	switch cfg.LogLevel {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	case "none":
+		return slog.New(slog.DiscardHandler), nil
+	case "":
+		level = slog.LevelError
+	default:
+		return nil, fmt.Errorf("invalid log level: %s", cfg.LogLevel)
+	}
+
+	if cfg.Json {
+		handler = slog.NewJSONHandler(writer, &slog.HandlerOptions{
+			Level:     level,
+			AddSource: cfg.LogLevel == "debug",
+		})
+	} else {
+		handler = slog.NewTextHandler(writer, &slog.HandlerOptions{
+			Level:     level,
+			AddSource: cfg.LogLevel == "debug",
+		})
+	}
+
+	return slog.New(handler), nil
+}
