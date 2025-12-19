91ffcbf62f8b1c9583aabdfce7749c5968bdf3b6
HT4w5 <ht4w5x@gmail.com>
2025-12-20 00:19:14 +0800

    feat(pool): implement Pool
    Pool manages ACL-rule generation logic.

---
+++ internal/pool/pool.go
@@ -0,0 +1,156 @@
+package pool
+
+import (
+	"fmt"
+	"net/netip"
+	"time"
+
+	"github.com/HT4w5/nyaago/internal/config"
+	"github.com/HT4w5/nyaago/pkg/db"
+	"github.com/HT4w5/nyaago/pkg/dto"
+	"go4.org/netipx"
+)
+
+var pool *Pool
+
+type Pool struct {
+	cfg               *config.Config
+	adapter           db.DBAdapter
+	clientWhitelist   *netipx.IPSet
+	resourceWhitelist map[string]struct{}
+	requestWhitelist  map[string]*netipx.IPSet
+}
+
+func GetPool(cfg *config.Config) (*Pool, error) {
+	if pool != nil {
+		return pool, nil
+	}
+
+	// Make DBAdapter
+	db, err := db.MakeDBAdapter(cfg.DB.Type, cfg.DB.Access)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get DBAdapter: %w", err)
+	}
+
+	p := &Pool{
+		cfg:     cfg,
+		adapter: db,
+	}
+
+	// Init whitelists
+	p.clientWhitelist, err = makeIPSet(cfg.Pool.ClientConfig.Whitelist)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make client whitelist: %w", err)
+	}
+
+	for _, v := range cfg.Pool.ResourceConfig.Whitelist {
+		p.resourceWhitelist[v] = struct{}{}
+	}
+
+	setBuilderMap := make(map[string]*netipx.IPSetBuilder)
+	for _, v := range cfg.Pool.RequestConfig.Whitelist {
+		prefix, err := netip.ParsePrefix(v.Prefix)
+		if err != nil {
+			return nil, fmt.Errorf("failed to make request whitelist: %w", err)
+		}
+		b, ok := setBuilderMap[v.URL]
+		if !ok {
+			b := &netipx.IPSetBuilder{}
+			setBuilderMap[v.URL] = b
+		}
+
+		b.AddPrefix(prefix)
+	}
+
+	for k, v := range setBuilderMap {
+		p.requestWhitelist[k], err = v.IPSet()
+		if err != nil {
+			return nil, fmt.Errorf("failed to make request whitelist: %w", err)
+		}
+	}
+
+	pool = p
+	return p, nil
+}
+
+func (p *Pool) ProcessRequest(req dto.Request) error {
+	// -- Filter with whitelist --
+	_, ok := p.resourceWhitelist[req.URL]
+	if ok || p.clientWhitelist.Contains(req.Client) {
+		return nil
+	}
+
+	set, ok := p.requestWhitelist[req.URL]
+	if ok && set.Contains(req.Client) {
+		return nil
+	}
+
+	currentTime := time.Now()
+
+	// -- Process client --
+	client, err := p.adapter.GetClient(req.Client)
+	if err != nil {
+		return fmt.Errorf("failed to get client %s: %w", req.Client, err)
+	}
+	if client.Addr.IsValid() {
+		// Update client attributes
+		client.TotalSent += req.BodySent
+		client.ExpiresOn = currentTime.Add(p.cfg.Pool.ClientConfig.TTL.Duration)
+	} else {
+		// Create new
+		client = db.Client{
+			Addr:      req.Client,
+			TotalSent: req.BodySent,
+			CreatedOn: currentTime,
+			ExpiresOn: currentTime.Add(p.cfg.Pool.ClientConfig.TTL.Duration),
+		}
+	}
+
+	err = p.adapter.PutClient(client)
+	if err != nil {
+		return fmt.Errorf("failed to put client %s: %w", client.Addr, err)
+	}
+
+	// -- Process resource --
+	resource, err := p.adapter.GetResource(req.URL)
+	if err != nil {
+		return fmt.Errorf("failed to get resource %s: %w", req.URL, err)
+	}
+	// Update resource attributes
+	resource.Size = max(resource.Size, req.BodySent)
+	resource.ExpiresOn = currentTime.Add(p.cfg.Pool.ResourceConfig.TTL.Duration)
+	resource.URL = req.URL
+
+	err = p.adapter.PutResource(resource)
+	if err != nil {
+		return fmt.Errorf("failed to put resource %s: %w", client.Addr, err)
+	}
+
+	// -- Process request --
+	request, err := p.adapter.GetRequest(req.Client, req.URL)
+	if request.Addr.IsValid() {
+		// Update request attributes
+		request.ExpiresOn = currentTime.Add(p.cfg.Pool.ResourceConfig.TTL.Duration)
+		request.TotalSent += req.BodySent
+		request.SendRatio = float64(request.TotalSent*24) / float64(resource.Size) / currentTime.Sub(request.CreatedOn).Hours() // Sent times per day
+		request.Occurrence++
+	} else {
+		// Create new
+		request = db.Request{
+			Addr:       req.Client,
+			URL:        req.URL,
+			TotalSent:  req.BodySent,
+			SendRatio:  1.,
+			Occurrence: 1,
+			CreatedOn:  currentTime,
+			ExpiresOn:  currentTime.Add(p.cfg.Pool.RequestConfig.TTL.Duration),
+		}
+	}
+
+	err = p.adapter.PutRequest(request)
+	if err != nil {
+		return fmt.Errorf("failed to put request %s: %w", client.Addr, err)
+	}
+
+	return nil
+}
---
+++ internal/pool/rules.go
@@ -0,0 +1,103 @@
+package pool
+
+import (
+	"fmt"
+	"net/netip"
+	"time"
+
+	"github.com/HT4w5/nyaago/pkg/db"
+	"github.com/HT4w5/nyaago/pkg/dto"
+	"go4.org/netipx"
+)
+
+func (p *Pool) GetRules() ([]dto.Rule, error) {
+	rules, err := p.adapter.ListRules()
+	if err != nil {
+		return []dto.Rule{}, fmt.Errorf("failed to list rules: %w", err)
+	}
+
+	var result []dto.Rule
+
+	for _, v := range rules {
+		result = append(result, dto.Rule{
+			Prefix:    v.Prefix,
+			Addr:      v.Addr,
+			URL:       v.URL,
+			ExpiresOn: v.ExpiresOn,
+		})
+	}
+
+	return result, nil
+}
+
+func (p *Pool) GetRuleSet() (*netipx.IPSet, error) {
+	rules, err := p.adapter.ListRules()
+	if err != nil {
+		return nil, fmt.Errorf("failed to list rules: %w", err)
+	}
+
+	var b netipx.IPSetBuilder
+	for _, v := range rules {
+		b.AddPrefix(v.Prefix)
+	}
+
+	return b.IPSet()
+}
+
+func (p *Pool) FlushExpired() error {
+	var err error
+	err = p.adapter.FlushExpiredClients()
+	if err != nil {
+		return fmt.Errorf("failed to flush expired clients: %w", err)
+	}
+	err = p.adapter.FlushExpiredRequests()
+	if err != nil {
+		return fmt.Errorf("failed to flush expired requests: %w", err)
+	}
+	err = p.adapter.FlushExpiredResources()
+	if err != nil {
+		return fmt.Errorf("failed to flush expired resources: %w", err)
+	}
+	err = p.adapter.FlushExpiredRules()
+	if err != nil {
+		return fmt.Errorf("failed to flush expired rules: %w", err)
+	}
+
+	return nil
+}
+
+func (p *Pool) BuildRules() error {
+	// Filter for malicious requests
+	requests, err := p.adapter.FilterRequests(p.cfg.Pool.SendRatioThreshold, p.cfg.Pool.RequestConfig.MaturationThreshold)
+	if err != nil {
+		return fmt.Errorf("failed to filter for requests: %w", err)
+	}
+
+	// Generate and store rules
+	currentTime := time.Now()
+	for _, v := range requests {
+		var prefixLength int
+		if v.Addr.Is4() {
+			prefixLength = p.cfg.Pool.BanPrefixLength.IPv4
+		} else if v.Addr.Is6() {
+			prefixLength = p.cfg.Pool.BanPrefixLength.Ipv6
+		} else {
+			// Drop invalid
+			continue
+		}
+
+		r := db.Rule{
+			Prefix:    netip.PrefixFrom(v.Addr, prefixLength),
+			Addr:      v.Addr,
+			URL:       v.URL,
+			ExpiresOn: currentTime.Add(p.cfg.Pool.RuleConfig.TTL.Duration),
+		}
+
+		err = p.adapter.PutRule(r)
+		if err != nil {
+			return fmt.Errorf("failed to put rule: %w", err)
+		}
+	}
+
+	return nil
+}
