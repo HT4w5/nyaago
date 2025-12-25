a24c235122861a6a705e4059858a38c9b7c25ee5
HT4w5 <ht4w5x@gmail.com>
2025-12-25 16:03:41 +0800

    feat(api): remove GET, PUT, DELETE rule
    Due to denylist using bigcache, these operations prove to be difficult to implement. Will refactor denylist in the future to mitigate this.

--- internal/server/api.go
+++ internal/server/api.go
@@ -1,9 +1,7 @@
 package server
 
 import (
-	"encoding/base64"
 	"net/http"
-	"net/netip"
 
 	"github.com/HT4w5/nyaago/pkg/dto"
 	"github.com/gin-gonic/gin"
@@ -36,109 +34,4 @@ func (s *Server) HandleGetRules(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, resp)
-}
-
-func (s *Server) HandleGetRule(c *gin.Context) {
-	decoded, err := base64.URLEncoding.DecodeString(c.Param("addr"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid base64url"})
-		return
-	}
-	addr, err := netip.ParseAddr(string(decoded))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid addr"})
-		return
-	}
-
-	rule, err := s.denylist.GetRule()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-	if !rule.Prefix.IsValid() {
-		c.JSON(http.StatusNotFound, dto.ErrorJSON{Error: "rule not found"})
-		return
-	}
-
-	c.JSON(http.StatusOK, rule.JSON())
-}
-
-func (s *Server) HandleDeleteRule(c *gin.Context) {
-	decoded, err := base64.URLEncoding.DecodeString(c.Param("prefix"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid base64url"})
-		return
-	}
-	prefix, err := netip.ParsePrefix(string(decoded))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid prefix"})
-		return
-	}
-
-	tx, err := s.db.Begin()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	err = tx.DelRule(prefix)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	c.Status(http.StatusNoContent)
-}
-
-func (s *Server) HandlePutRule(c *gin.Context) {
-	decoded, err := base64.URLEncoding.DecodeString(c.Param("prefix"))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid base64url"})
-		return
-	}
-	prefix, err := netip.ParsePrefix(string(decoded))
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid prefix"})
-		return
-	}
-
-	var ruleJSON dto.RuleJSON
-	if err := c.ShouldBindJSON(&ruleJSON); err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid rule"})
-		return
-	}
-
-	rule, err := ruleJSON.ToObject()
-	if err != nil {
-		c.JSON(http.StatusBadRequest, dto.ErrorJSON{Error: "invalid rule"})
-		return
-	}
-
-	rule.Prefix = prefix
-
-	tx, err := s.db.Begin()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	err = tx.PutRule(rule)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, dto.MakeErrorJSON(err))
-		return
-	}
-
-	c.Status(http.StatusNoContent)
 }
